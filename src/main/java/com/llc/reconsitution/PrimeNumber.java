86a0bacde9a52af2614e194d41cdb55d5142f320
lilingchen <lingchen.li@moji.com>
2017-10-19 19:24:09 +0800

    prime number fisrt commit

---
+++ src/main/java/com/llc/reconsitution/PrimeNumber.java
@@ -0,0 +1,12 @@
+package com.llc.reconsitution;
+
+/**
+ * 
+ * @author Lingchen Li
+ *  在一般领域，对正整数n，如果用2到  之间的所有整数去除，均无法整除，则n为质数。
+ * 质数大于等于2 不能被它本身和1以外的数整除
+ */
+public class PrimeNumber {
+	
+	
+}
