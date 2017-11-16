789de40f50b2d907ae0d4681e4083265f2cacdd1
AI_skynet <1603100619@qq.com>
2017-11-17 00:10:16 +0800

    fix

--- src/main/java/com/llc/arithmetic/Explainer.java
+++ src/main/java/com/llc/arithmetic/Explainer.java
@@ -6,40 +6,39 @@ public class Explainer {
 
 	public static void main(String[] args) {
 
-		String expression = " ( 20/(20+40))";
+		String expression = " ( 2/10)";
 		Algs4Stack<Double> operandStack = new Algs4Stack<Double>();
 		Algs4Stack<String> operatorStack = new Algs4Stack<String>();
-        
+
 		for (int i = 1; i <= expression.length(); i++) {
-			String current = expression.substring(i-1, i);
-			if(!current.matches("\\s")){
-				if(current.matches("[\\+\\-\\*\\/]{1}")){
+			String current = expression.substring(i - 1, i);
+			if (!current.matches("\\s")) {
+				if (current.equals("(")) {
+
+				} else if (current.matches("[\\+\\-\\*\\/]{1}")) {
 					operatorStack.push(current);
-				}
-				if(current.matches("[0-9]{1}")){
-					operandStack.push(Double.parseDouble(current));
-				}
-				if(current.equals(")")){
+				} else if (current.equals(")")) {
 					String op = operatorStack.pop();
 					Double v = operandStack.pop();
-					if(op.equals("+")){
+					if (op.equals("+")) {
 						v = operandStack.pop() + v;
-					}else if(op.equals("-")){
+					} else if (op.equals("-")) {
 						v = operandStack.pop() - v;
-					}else if(op.equals("*")){
+					} else if (op.equals("*")) {
 						v = operandStack.pop() * v;
-					}else if(op.equals("/")){
+					} else if (op.equals("/")) {
 						v = operandStack.pop() / v;
-					}	
+					}
 					operandStack.push(v);
+				} else {
+					operandStack.push(Double.parseDouble(current));
 				}
 			}
 		}
-		
-	   System.out.println(operandStack.pop());
-			
-//			.matches("[0-9]{1}");
-	
+
+		System.out.println(operandStack.pop());
+
+		// .matches("[0-9]{1}");
 
 	}
 }
